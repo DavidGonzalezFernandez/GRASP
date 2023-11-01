fa22aab806401553aa35833649b017a32f535d08
David González Fernández <david.g.f.2+github_hq7mbc23i9@gmail.com>
2023-11-01 16:39:38 +0100

    Solve bugs and implement focus on code segments

--- GRASP.py
+++ GRASP.py
@@ -39,19 +39,48 @@ def display_problem(self):
 
     self.wait()
 
+LINE_HEIGHT = 0.5
+get_rectangle_while = lambda : Rectangle(height=LINE_HEIGHT, width=4.8).move_to(2.15*LEFT + 0.875*UP)
+get_rectangle_construct = lambda : Rectangle(height=LINE_HEIGHT, width=5.4).move_to(1.14*LEFT + 0.425*UP)
+get_rectangle_whole_if_feasible = lambda : Rectangle(height=LINE_HEIGHT*2 - 0.1, width=5.45).move_to(0.175*DOWN + 1.15*LEFT)
+get_rectangle_local_search = lambda : Rectangle(height=LINE_HEIGHT, width=5.8).move_to(0.8*DOWN + 0.95*LEFT)
+
+def show_code_grasp_focus_while(self):
+    rect = get_rectangle_while()
+    img = show_code_grasp(self, rect)
+    self.wait()
+    self.play(FadeOut(rect), FadeOut(img))
+
+def show_code_grasp_focus_construct(self):
+    rect = get_rectangle_construct()
+    img = show_code_grasp(self, rect)
+    self.wait()
+    self.play(FadeOut(rect), FadeOut(img))
+
+def show_code_grasp_focus_feasible(self):
+    rect = get_rectangle_whole_if_feasible()
+    img = show_code_grasp(self, rect)
+    self.wait()
+    self.play(FadeOut(rect), FadeOut(img))
+
+def show_code_grasp_focus_search(self):
+    rect = get_rectangle_local_search()
+    img = show_code_grasp(self, rect)
+    self.wait()
+    self.play(FadeOut(rect), FadeOut(img))
+
 
 """Given the shown GRASP code it uses rectangles to focus on specific parts of the code"""
 def explain_code_grasp(self):
     image = show_code_grasp(self)
-    line_height = 0.5
-
-    rectangle_while = Rectangle(height=line_height, width=4.8).move_to(2.15*LEFT + 0.875*UP)
-    rectangle_construct = Rectangle(height=line_height, width=5.4).move_to(1.14*LEFT + 0.425*UP)
-    rectangle_whole_if_feasible = Rectangle(height=line_height*2 - 0.1, width=5.45).move_to(0.175*DOWN + 1.15*LEFT)
-    rectangle_local_search = Rectangle(height=line_height, width=5.8).move_to(0.8*DOWN + 0.95*LEFT)
+    
+    rectangle_while = get_rectangle_while()
+    rectangle_construct = get_rectangle_construct()
+    rectangle_whole_if_feasible = get_rectangle_whole_if_feasible()
+    rectangle_local_search = get_rectangle_local_search()
 
     # Focus con construct
-    self.add(rectangle_construct)
+    self.play(Create(rectangle_construct))
     self.wait()
 
     # Focus on repair
@@ -71,11 +100,14 @@ def explain_code_grasp(self):
 
 
 """Shows the code for GRASP"""
-def show_code_grasp(self):
+def show_code_grasp(self, additional_mobjects=[]):
     DOWN_SHIFT = 0
     image = ImageMobject("my_media//GRASP_code_snippet_v3.png").shift(DOWN_SHIFT * DOWN)
 
-    self.play(FadeIn(image))
+    self.play(
+        FadeIn(image),
+        *[FadeIn(o) for o in additional_mobjects]    
+    )
 
     self.wait()
 
@@ -84,8 +116,8 @@ def show_code_grasp(self):
 
 
 """"""
+# TODO: implement
 def explain_restricted_candidate_list(self):
-    # TODO: implement
 
     # Explain different ways of filterin
     #text1 = Text(r"[c_min, c_min + α * (c_max - c_min)]")
@@ -110,7 +142,7 @@ def explain_alpha(self):
 
     get_text = lambda : Text(f"[{MIN_C:.2f}, {(MIN_C+alpha.get_value()*(MAX_C-MIN_C)):.2f}]").shift(5*LEFT+2.5*UP).scale(1/2)
     # The text showing the MIN and MAX values allowed within the range
-    text = get_text()
+    range_text = get_text()
     
     x_start_line, x_end_line, y_line = -6.5, -3.5, 1.5
     # The line that will be traveled by the Dot
@@ -120,10 +152,22 @@ def explain_alpha(self):
     # Dot to visually see where the alpha is located
     dot = get_dot()
     
-    get_lambda_text = lambda : Text(f"{(alpha.get_value()):.2f}").scale(1/2)
+    get_lambda_text = lambda : Text(f"{(alpha.get_value()):.2f}").scale(1/2).next_to(dot, DOWN)
     # Text displaying the value of alpha
     alpha_text = get_lambda_text()
-    alpha_text.next_to(dot, DOWN)
+
+    # All the objects to be displayed
+    objects = [box, range_text, alpha_text, line, dot]
+    self.play(
+        *[FadeIn(o) for o in objects],
+        *[
+            FadeToColor(problem["DOTS_mobject"][i], color=RCL_COLOR)
+            for i in DISTANCES.keys()
+            if (DISTANCES[i] <= (MIN_C+alpha.get_value()*(MAX_C-MIN_C)))
+        ]
+    )
+
+    self.wait()
 
     for (i,d) in DISTANCES.items():
         # Update the colors of the points if they can be included in RCL
@@ -131,17 +175,12 @@ def explain_alpha(self):
             x.set_color(DEFAULT_COLOR if (DISTANCES[i] > (MIN_C+alpha.get_value()*(MAX_C-MIN_C))) else RCL_COLOR)
         problem["DOTS_mobject"][i].add_updater(the_updater)
 
-    def update_lambda_text(z):
-        z.become(get_lambda_text())
-        z.next_to(dot, DOWN)
-
-    # All the objects to be displayed
-    objects = [box, text, alpha_text, line, dot]
-    self.play(*[FadeIn(o) for o in objects])
+    def update_lambda(point):
+        point.become(get_dot())
+        alpha_text.become(get_lambda_text())
     
-    text.add_updater(lambda z: z.become(get_text()))
-    dot.add_updater(lambda z : z.become(get_dot()))
-    alpha_text.add_updater(update_lambda_text)
+    dot.add_updater(update_lambda)
+    range_text.add_updater(lambda z: z.become(get_text()))
 
     # From center to MAX
     self.play(alpha.animate.set_value(MAX_ALPHA), run_time=2)
@@ -156,9 +195,11 @@ def explain_alpha(self):
     self.wait()
 
     for i in DISTANCES.keys():
-        d = problem["DOTS_mobject"][i]
-        d.clear_updaters()
-        d.set_color(DEFAULT_COLOR)
+        problem["DOTS_mobject"][i].clear_updaters()
+    
+    range_text.clear_updaters()
+    dot.clear_updaters()
+    alpha_text.clear_updaters()
 
     # Restore view to match previous
     self.play(
@@ -208,6 +249,9 @@ def construct_initial_solution(self):
     # Pause to explain the RCL
     explain_restricted_candidate_list(self)
 
+    NUM_SHOW_RCL = 2
+    NUM_CREATE_LINES_SLOW = NUM_SHOW_RCL + 0
+
     # Repeat until all points have been visited
     while len(problem["DOTS_visited"]) != len(problem["DOTS_mobject"]):
         last_x, last_y = problem["DOTS_coord"][ problem["DOTS_visited"][-1] ]
@@ -219,7 +263,7 @@ def construct_initial_solution(self):
         restricted_candidate_list = get_restricted_candidate_list(distances, alpha)
 
         # Animation to show candidate_list
-        if len(problem["DOTS_visited"]) <= 2:
+        if len(problem["DOTS_visited"]) <= NUM_SHOW_RCL:
             the_dots = [d for (i,d) in enumerate(problem["DOTS_mobject"]) if i in restricted_candidate_list.keys()]
             self.play(*[FadeToColor(d, color=RCL_COLOR) for d in the_dots])
             self.wait()
@@ -234,7 +278,8 @@ def construct_initial_solution(self):
         self.play(
             FadeToColor(problem["DOTS_mobject"][problem["DOTS_visited"][-1]], color=VISITED_COLOR),
             FadeToColor(problem["DOTS_mobject"][index], color=LAST_VISITED_COLOR),
-            Create(line)
+            Create(line),
+            run_time = 1 if len(problem["DOTS_visited"]) <= NUM_CREATE_LINES_SLOW else 0.5
         )
 
         problem["EDGES_mobject"].append(line)
@@ -253,31 +298,56 @@ def construct_initial_solution(self):
     self.wait()
 
 
+"""Shows the title to do the introduction"""
+def show_introduction(self):
+    title_whole_name = Paragraph("Greedy", "Randomized", "Adaptive", "Search", "Procedure")
+    self.add(title_whole_name)
+    self.wait()
+
+    title_GRASP = Text("GRASP")
+    self.play(ReplacementTransform(title_whole_name, title_GRASP))
+    self.wait()
+
+    # TODO: pensar qué más poner
+
+    self.play(FadeOut(title_GRASP))
+    self.wait()
+
+
+
 
 class GRASP(Scene):
     def construct(self):
         # TODO: Show the name and meaning
+        show_introduction(self)
 
         # Show the general code for GRASP
         explain_code_grasp(self)
         self.wait()
 
         # Introduce the problem to solve
+        # TODO: show problem's title
         build_problem(self)
         display_problem(self)
         self.wait()
 
-        # Show the general code for construction
-        # TODO: Show the general code and focus on construction
+        # Show the general code and focus on construction
+        show_code_grasp_focus_construct(self)
+        # TODO: Show the code for construction
+        # Visualize construction
         construct_initial_solution(self)
 
-        # TODO: Show the idea behind repair
         # Show the general code and focus on repair
+        show_code_grasp_focus_feasible(self)
+        # TODO: Show the idea behind repair
+        # TODO: Visualize repair
 
-        # TODO: Show local search
         # Show the general code and focus on local search
+        show_code_grasp_focus_search(self)
+        # TODO: Show the code for local search
+        # TODO: Visualize local search
 
-        # TODO: redo explanation of code
+        # TODO: Redo explanation of code
 
         # TODO: Show results and modifications of parameters
 
