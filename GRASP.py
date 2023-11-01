090f1975205f60c54b2a616db8a3c01b085ef9c7
David González Fernández <david.g.f.2+github_hq7mbc23i9@gmail.com>
2023-11-01 14:05:44 +0100

    Basic structure for the video

---
+++ GRASP.py
@@ -0,0 +1,19 @@
+from manim import *
+
+class ExplainConstructionOfCandidateList(Scene):
+    def construct(self):
+        # TODO: Show the name and meaning
+
+        # TODO: Show the general code for GRASP
+
+        # TODO: Introduce the problem to solve
+
+        # TODO: Show the general code for construction
+
+        # TODO: Show the idea behind repair
+
+        # TODO: Show local search
+
+        # TODO: Show results and modifications of parameters
+
+        # TODO: Show extensions and improvements
